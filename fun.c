afc367e6f31832b24c6c50e18b0979a9f2eb3b86
Prayatna <prayatnasapkota0@gmail.com>
2025-01-30 11:39:00 +0545

    Number Guessing Game (C Program)
    JUST A PROJECT I MADE TO PUT ON MY SITE
    AFTER SOMETHING I WILL REMOVE COZ ITS TOO SIMPLE!!!
    AFTER ADDING SOMETHING I AM WORKING ON I WILL REMOVE IT!!

---
+++ fun.c
@@ -0,0 +1,31 @@
+// You can opy no worries!!!
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+ 
+
+int main() {
+    int secret, guess, attempts = 0;
+
+    srand(time(0));  
+    secret = rand() % 100 + 1;  
+
+    printf("Guess the number (1-100):\n");
+
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        attempts++;
+
+        if (guess > secret) {
+            printf("Too high! Try again.\n");
+        } else if (guess < secret) {
+            printf("Too low! Try again.\n");
+        } else {
+            printf("Congratulations! You guessed it in %d attempts.\n", attempts);
+        }
+    } while (guess != secret);
+
+    return 0;
+}
